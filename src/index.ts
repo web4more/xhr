afb844a75dbee41b14efa5c47fca8ea2e161e489
Jacob Hummer <jcbhmr@outlook.com>
2023-04-18 21:39:27 -0500

    Fiddle with global type exposure

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,26 @@
-import XMLHttpRequestEventTarget from "./XMLHttpRequestEventTarget.ts"
-import XMLHttpRequestUpload from "./XMLHttpRequestUpload.ts"
-import XMLHttpRequestResponseType from "./XMLHttpRequestResponseType.ts"
+import XMLHttpRequestEventTarget from "./XMLHttpRequestEventTarget.ts";
+import XMLHttpRequestUpload from "./XMLHttpRequestUpload.ts";
+import XMLHttpRequestResponseType from "./XMLHttpRequestResponseType.ts";
 import XMLHttpRequest from "./XMLHttpRequest.ts";
-import ProgressEvent from "./ProgressEvent.ts"
-import ProgressEventInit from "./ProgressEventInit.ts"
+import ProgressEvent from "./ProgressEvent.ts";
+import ProgressEventInit from "./ProgressEventInit.ts";
 
+type XMLHttpRequestEventTarget_ = typeof XMLHttpRequestEventTarget;
+type XMLHttpRequestUpload_ = typeof XMLHttpRequestUpload;
+type XMLHttpRequestResponseType_ = XMLHttpRequestResponseType;
+type XMLHttpRequest_ = typeof XMLHttpRequest;
+type ProgressEvent_ = typeof ProgressEvent;
+type ProgressEventInit_ = ProgressEventInit_;
 declare global {
-  var XMLHttpRequestEventTarget: typeof XMLHttpRequestEventTarget
-  var XMLHttpRequestUpload: typeof XMLHttpRequestUpload
-  type XMLHttpRequestResponseType = XMLHttpRequestResponseType;
-  var XMLHttpRequest: typeof XMLHttpRequest
-  var ProgressEvent: typeof ProgressEvent;
-  type ProgressEventInit = ProgressEventInit;
+  var XMLHttpRequestEventTarget: XMLHttpRequestEventTarget_;
+  var XMLHttpRequestUpload: XMLHttpRequestUpload_;
+  type XMLHttpRequestResponseType = XMLHttpRequestResponseType_;
+  var XMLHttpRequest: XMLHttpRequest_;
+  var ProgressEvent: ProgressEvent_;
+  type ProgressEventInit = ProgressEventInit_;
 }
 
 globalThis.XMLHttpRequestEventTarget = XMLHttpRequestEventTarget;
-globalThis.XMLHttpRequestUpload = XMLHttpRequestUpload
+globalThis.XMLHttpRequestUpload = XMLHttpRequestUpload;
 globalThis.XMLHttpRequest = XMLHttpRequest;
 globalThis.ProgressEvent = ProgressEvent;
